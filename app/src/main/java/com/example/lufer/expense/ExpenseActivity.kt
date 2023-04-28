069ba7e80d329de269325df3d9c47639eb71f4bc
FatihUtkuKara <fatihutkukara@gmail.com>
2023-04-28 18:16:26 +0300

    Update ExpenseActivity.kt

--- app/src/main/java/com/example/lufer/expense/ExpenseActivity.kt
+++ app/src/main/java/com/example/lufer/expense/ExpenseActivity.kt
@@ -2,11 +2,36 @@ package com.example.lufer.expense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lufer.R
+import com.example.lufer.databinding.ActivityExpenseBinding
+import com.example.lufer.income.Incomes
+import java.util.ArrayList
 
 class ExpenseActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityExpenseBinding
+    private lateinit var expenseList : ArrayList<Expenses>
+    private lateinit var adapter: ExpenseRvAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expense)
+        binding = ActivityExpenseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.expenseRv.setHasFixedSize(true)
+
+        binding.expenseRv.layoutManager = LinearLayoutManager(this)
+
+        val i1 = Expenses(1,"1")
+        val i2 = Expenses(2,"2")
+
+        expenseList = ArrayList<Expenses>()
+        expenseList.add(i1)
+        expenseList.add(i2)
+
+        adapter = ExpenseRvAdapter(this,expenseList)
+
+        binding.expenseRv.adapter = adapter
+
     }
 }
